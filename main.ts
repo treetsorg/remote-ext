5e87af5c63024fe8316ec39bfacd6c775f6b1b4d
treets <mail@andreas-theis.net>
2020-01-13 10:40:25 +0100

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+/**
+ * ...
+ */
+//% color=#dd2b49 weight=100 icon="\uf2db" block="Bundled"
+namespace bundled {
+    //% block
+    export function test(): void {
+        tools.log("test() from bundled extension");
+    }
+}
