bcac01c2b7c2c6065bc3631518b167b37bc40032
treets <mail@andreas-theis.net>
2020-01-13 14:50:27 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 /**
  * ...
  */
-//% color=#ddff49 weight=100 icon="\uf2db" block="Bundled"
+//% color=#ddff49 weight=100 icon="\uf2db" block="Remote"
 namespace remote {
     //% block
     export function test(): void {
