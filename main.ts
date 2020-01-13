bc8553d83afdc495a9bdd3bd1fc902b6d4bfde43
treets <mail@andreas-theis.net>
2020-01-13 14:33:50 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /**
  * ...
  */
-//% color=#dd2b49 weight=100 icon="\uf2db" block="Bundled"
-namespace bundled {
+//% color=#ddff49 weight=100 icon="\uf2db" block="Bundled"
+namespace remote {
     //% block
     export function test(): void {
         tools.log("test() from bundled extension");
